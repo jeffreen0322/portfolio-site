9fb860e057b4116dedcc5cde6008b56e3b2c6d97
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-11-28 15:44:56 -0800

    Added assets for ticker + skills.js

---
+++ src/components/technologies/skills/skills.js
@@ -0,0 +1,31 @@
+import htmlLogo from "./assets/HTML5_logo_black.png";
+import cssLogo from "./assets/CSS3_logo_and_wordmark.png";
+import javascriptLogo from "./assets/Javascript_badge.png";
+import typescriptLogo from "./assets/typescript.png";
+import reactLogo from "./assets/React.png";
+import angularLogo from "./assets/angular.png";
+import ionicLogo from "./assets/ionic.png";
+import mysqlLogo from "./assets/mysql_PNG9.png";
+import pythonLogo from "./assets/Python-logo-notext.png";
+import cLogo from "./assets/C_Logo.png";
+import cppLogo from "./assets/C_logo (1).png";
+import javaLogo from "./assets/Java-logo.png";
+
+export const userInfo = {
+  skills: [
+    { name: "html", image: { htmlLogo } },
+    { name: "css", image: { cssLogo } },
+    { name: "javascript", image: { javascriptLogo } },
+    { name: "typescript", image: { typescriptLogo } },
+    { name: "react", image: { reactLogo } },
+    { name: "angular", image: { angularLogo } },
+    { name: "ionic", image: { ionicLogo } },
+    { name: "mysql", image: { mysqlLogo } },
+    { name: "python", image: { pythonLogo } },
+    { name: "c", image: { cLogo } },
+    { name: "c++", image: { cppLogo } },
+    { name: "java", image: { javaLogo } },
+    { name: "git", image: "/" },
+    { name: "figma", image: "/" },
+  ],
+};
