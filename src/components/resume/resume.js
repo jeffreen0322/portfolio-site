07067b42dd4bd547a09a5c79c207086c7a3d4cd0
jeffreen0322 <jeffreyenguyen@gmail.com>
2024-12-11 12:32:07 -0800

    Adjusted navigation size and Logo

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -8,27 +8,29 @@ export default function Resume() {
   return (
     <div className="resume-section">
       <div className="resume-container" id="resume">
-        <Link
-          to={resumeFile}
-          target="_blank"
-          download="Jeffrey Ethan Nguyen.pdf"
-          style={{ textDecoration: "none" }}
-        >
-          <button className="download">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              className="bi bi-download"
-              viewBox="0 0 16 16"
-            >
-              <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
-              <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
-            </svg>{" "}
-            Download Resume
-          </button>
-        </Link>
+        <div className="dload-container">
+          <Link
+            to={resumeFile}
+            target="_blank"
+            download="Jeffrey Ethan Nguyen.pdf"
+            style={{ textDecoration: "none" }}
+          >
+            <button className="download">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                fill="currentColor"
+                className="bi bi-download"
+                viewBox="0 0 16 16"
+              >
+                <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
+                <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
+              </svg>{" "}
+              Download Resume
+            </button>
+          </Link>
+        </div>
         <header style={{ textAlign: "center", marginBottom: "20px" }}>
           <h3>Jeffrey Ethan Nguyen</h3>
           <div>
@@ -114,8 +116,8 @@ export default function Resume() {
                 JavaScript.
               </li>
               <li>
-                Hosted on Vercel, featuring projects, repositories,
-                socials, and skills.
+                Hosted on Vercel, featuring projects, repositories, socials, and
+                skills.
               </li>
             </ul>
           </div>
